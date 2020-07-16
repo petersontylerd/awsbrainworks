345e5251a8b45e4a304fd78c50bcb5ff75856518
petersontylerd <petersontylerd@gmail.com>
2020-07-16 16:20:23 -0500

    fix home path ref in pyenv setup file

--- awsbrainworks/compute/setup/pyenv/pyenv_setup.sh
+++ awsbrainworks/compute/setup/pyenv/pyenv_setup.sh
@@ -4,9 +4,11 @@ if ! command -v pyenv 1>/dev/null; then
 echo '
 
 # pyenv env variables and inits
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_ROOT=\$HOME/.pyenv
+export PATH=\$PYENV_ROOT/bin:\$PATH
+' >> ~/.bashrc
 
+echo '
 if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init -)"
 fi
