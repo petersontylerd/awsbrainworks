eb6019ec614524b9253b4ac5997aa48c03fd377e
Tyler Peterson <petersontylerd@gmail.com>
2020-07-17 19:41:41 +0000

    fix jupyter extension file

--- awsbrainworks/compute/setup/jupyter/setup_extensions.sh
+++ awsbrainworks/compute/setup/jupyter/setup_extensions.sh
@@ -3,5 +3,3 @@ jupyter nbextension enable code_font_size/code_font_size
 jupyter nbextension enable highlight_selected_word/main
 jupyter nbextension enable collapsible_headings/main
 jupyter nbextension enable codefolding/main
-jupyter nbextension enable /main
-jupyter nbextension enable /main
